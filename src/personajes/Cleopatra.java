6312f6cfdbb13c5fd5ed80622420920d0136bf2e
Victor Cebollada <vic.cebollada@gmail.com>
2024-11-28 18:39:54 +0100

    Victor añade descripcion de Cleopatra

--- src/personajes/Cleopatra.java
+++ src/personajes/Cleopatra.java
@@ -1,25 +1,25 @@
 package personajes;
 
 /**
- * Victor Cebollada
+ * Luca Soler
  */
-public class Cleopatra {
+public class NikolaTesla {
     public static String nombre;
     public static String descripcionFisica;
     public static String historia;
     public static String fraseTipica;
 
     static {
-        Cleopatra.nombre = "";
-        Cleopatra.descripcionFisica = "";
-        Cleopatra.historia = "";
-        Cleopatra.fraseTipica = "";
+        NikolaTesla.nombre = "Nikola Tesla";
+        NikolaTesla.descripcionFisica = "Delgado, de cabello oscuro peinado hacia atrás, vestido con traje formal.";
+        NikolaTesla.historia = "Inventor y científico serbio, pionero de la corriente alterna y obsesionado con la energía y el magnetismo.";
+        NikolaTesla.fraseTipica = "La electricidad es el alma del universo.";
     }
 
     public static void main(String[] args) {
-        System.out.println(Cleopatra.nombre + "\n");
-        System.out.println(Cleopatra.descripcionFisica + "\n");
-        System.out.println(Cleopatra.historia + "\n");
-        System.out.println(Cleopatra.fraseTipica + "\n");
+        System.out.println(NikolaTesla.nombre + "\n");
+        System.out.println(NikolaTesla.descripcionFisica + "\n");
+        System.out.println(NikolaTesla.historia + "\n");
+        System.out.println(NikolaTesla.fraseTipica + "\n");
     }
 }
