acbce21e9c1ef073bcda545b82e13cd0062092e2
jxrgxx <24.damt.jleileon@fundacionmontessori.com>
2024-12-05 18:44:12 +0100

    Leonardo habla

--- src/Dialogo.java
+++ src/Dialogo.java
@@ -25,7 +25,8 @@ public class Dialogo {
         System.out.println("Leonardo: (Examinando la tarjeta) Qué diseño tan curioso. Quizás algún código esté escondido aquí.");
         System.out.println("Cleopatra: En mi tiempo, el cálculo y la lógica eran claves para la victoria. Ese ábaco me intriga.");
         System.out.println("Tesla: Mis experimentos buscaban crear energía ilimitada. Tal vez hemos liberado algo más grande.");
-        System.out.println("Juana: Dios guía mi espada, pero también mi mente. Pienso que debemos unirnos para resolver esto.")
+        System.out.println("Juana: Dios guía mi espada, pero también mi mente. Pienso que debemos unirnos para resolver esto.");
+        System.out.println("Leonardo: Tesla, explícanos cómo estas fuerzas invisibles pueden unirnos.");
         System.out.println(Dialogo.Desenlace);
     }   
 }
